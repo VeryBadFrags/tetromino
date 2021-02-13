a871deb9993ae4db2acd6288f1e0f982443094c5
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-12 23:55:32 -0800

    Update node packages

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,7 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
     mount: {
         "src": { url: "/" },
