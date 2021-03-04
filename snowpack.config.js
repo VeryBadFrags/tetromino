1e889dda2d0eb264b38d19cd35ecb578c514a616
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-03-03 18:01:36 -0800

    Run Prettier

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,16 +3,19 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-    mount: {
-        "src": { url: "/" },
-        "assets": { url: "/", static: true },
-    },
-    plugins: [
-        ["@snowpack/plugin-sass", {
-            style: "compressed",
-            sourceMap: false,
-        }],
-        ['@snowpack/plugin-typescript'],
-        ["@snowpack/plugin-optimize"],
+  mount: {
+    src: { url: "/" },
+    assets: { url: "/", static: true },
+  },
+  plugins: [
+    [
+      "@snowpack/plugin-sass",
+      {
+        style: "compressed",
+        sourceMap: false,
+      },
     ],
+    ["@snowpack/plugin-typescript"],
+    ["@snowpack/plugin-optimize"],
+  ],
 };
